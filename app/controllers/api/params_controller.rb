c9948a3f625570ea436726474a36a1730c10f1bd
Sam Tanner <Samuel.m.s.tanner@gmail.com>
2021-03-01 14:35:39 -0800

    corrects where method is located, not in application controller

--- app/controllers/api/params_controller.rb
+++ app/controllers/api/params_controller.rb
@@ -1,2 +1,7 @@
 class Api::ParamsController < ApplicationController
+  def all_caps_action
+    input_message = params[:my_phrase]
+    @output_message = input_message.upcase
+    render "single_output.json.jb"
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, if: -> { request.format.html? }
 
-  def all_caps_action
-    input_message = params[:my_phrase]
-    @output_message = input_message.upcase
-    render "single_output.json.jb"
-  end
+
 end
